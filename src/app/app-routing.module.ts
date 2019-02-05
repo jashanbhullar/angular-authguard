a85f2c62279272ce6646c04f3bcf7988d2ca0dba
jashanbhullar <jsbhullar96@gmail.com>
2019-02-05 23:08:37 +0530

    - Fixed imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
-import { NotFoundComponent } from './component/not-found/not-found.component';
-import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '@/components/login/login.component';
+import { NotFoundComponent } from '@/component/not-found/not-found.component';
+import { HomeComponent } from '@/components/home/home.component';
 import { AuthGuard } from '@/guard/auth-guard.service';
 
 const routes: Routes = [
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from '@/app-routing.module';
+import { AppComponent } from '@/app.component';
+import { LoginComponent } from '@/components/login/login.component';
+import { HomeComponent } from '@/components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
-import { NotFoundComponent } from './component/not-found/not-found.component';
+import { NotFoundComponent } from '@/component/not-found/not-found.component';
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, HomeComponent, NotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
